5a2694187f11f38d86486fc81b4b115c2f1ebab9
Gabriele Puliti <gabriele.puliti@gmail.com>
2018-11-16 15:49:46 +0100

    setup some dependencies

--- scripts/atom.sh
+++ scripts/atom.sh
@@ -1,4 +1,9 @@
 # Atom Text Editor - https://atom.io/
+
+# dependencies
+sudo apt install apt-transport-https
+
+# atom installer
 curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt-get update
