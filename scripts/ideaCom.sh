86183f85950715ef6488978d0a52e92b83809b21
Gabriele Puliti <gabriele.puliti@gmail.com>
2018-11-27 21:12:47 +0100

    Edit idea community edition script

--- scripts/ideaCom.sh
+++ scripts/ideaCom.sh
@@ -4,5 +4,5 @@ wget -O idea.tar.gz https://download.jetbrains.com/idea/ideaIC-2018.2.5.tar.gz
 tar zxvf idea.tar.gz
 sudo rm -r idea.tar.gz
 mv idea-* idea
-cd idea/bin
-./idea.sh
+echo "You can find IntelliJ IDEA in your home folder"
+echo "To use you can simply run idea.sh in idea/bin folder"
