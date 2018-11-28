6299ff02b354362f5d6a984d545f1aeedbb99e28
Gabriele Puliti <gabriele.puliti@gmail.com>
2018-11-28 16:12:46 +0100

    minor update to mailspring script

--- scripts/mailspring.sh
+++ scripts/mailspring.sh
@@ -1,5 +1,4 @@
 # The best mail client - https://getmailspring.com/
 wget -O mailspring.deb https://updates.getmailspring.com/download?platform=linuxDeb
 sudo dpkg -i mailspring.deb
-sudo apt --fix-broken install
 rm -r mailspring.deb
