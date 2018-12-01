64e9a2339ec0f82b8a008fa0c44dc94f47989b75
Gabriele Puliti <gabriele.puliti@gmail.com>
2018-12-01 18:31:41 +0100

    create a simbolic link to idea and pycharm, with this change this two software can be run with a terminal without give the path

--- scripts/pycharmCom.sh
+++ scripts/pycharmCom.sh
@@ -4,5 +4,4 @@ wget -O pycharm.tar.gz https://download.jetbrains.com/python/pycharm-community-2
 tar zxvf pycharm.tar.gz
 sudo rm -r pycharm.tar.gz
 mv pycharm-* pycharm
-echo "You can find Pycharm in your home folder"
-echo "To use you can simply run pycharm.sh in pycharm/bin folder"
+sudo ln -s ~/pycharm/bin/pycharm.sh /usr/local/bin/pycharm
