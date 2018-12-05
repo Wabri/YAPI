e4f625458e00f99f1d01ec955a0d84ff88a3583b
Gabriele Puliti <gabriele.puliti@gmail.com>
2018-12-05 10:38:56 +0100

    Minor changes

--- scripts/rambox.sh
+++ scripts/rambox.sh
@@ -1,9 +1,5 @@
 # Rambox workspace browser - https://rambox.pro
-
-#dependencies
 sudo apt install gconf2
-
-#installer ranbox
 wget -O rambox.deb https://github.com/ramboxapp/community-edition/releases/download/0.6.2/Rambox-0.6.2-linux-amd64.deb
 sudo dpkg -i rambox.deb
 rm -r rambox.deb
