95035927bcc8debe348f3e31fd8960d168b9a97c
Gabriele Puliti <gabriele.puliti@gmail.com>
2018-12-05 10:45:43 +0100

    Created soft link for eclipse installer

--- scripts/eclipseInstaller.sh
+++ scripts/eclipseInstaller.sh
@@ -3,3 +3,4 @@ cd ~
 wget -O eclipseInst.tar.gz ftp.fau.de/eclipse/oomph/products/eclipse-inst-linux64.tar.gz
 tar zxvf eclipseInst.tar.gz
 sudo rm -r eclipseInst.tar.gz
+ln -s ~/eclipse-installer/eclipse-inst /usr/local/bin/eclipse-inst
