ad5c1e6f4026f86f4f529e251c768e4de867fe59
Gabriele Puliti <gabriele.puliti@gmail.com>
2018-12-09 20:31:17 +0100

    more refactor

--- scriptloader.py
+++ scriptloader.py
@@ -1,12 +1,7 @@
-import glob
-import os
-import pickle
-
-packages_binary_file = "packages.bin"
-
 
 def load_packages_from_file(binary_file):
     """Load packages list from binary."""
+    import pickle
     with open(binary_file, "rb") as binary_packages:
         packages_loaded = pickle.load(binary_packages)
         print("[LOG] {} Package load from {}".format(
@@ -14,8 +9,34 @@ def load_packages_from_file(binary_file):
     return packages_loaded
 
 
-def load_packages_from_directory(directory):
+def get_package_info(info):
+    """Get package description and url from info."""
+    description = ""
+    url = ""
+    info = info.strip("\n").strip("# ")
+    for character in info:
+        if character == "-":
+            break
+        description += character
+    precedent_character = ""
+    get_url = False
+    for character in info:
+        if not get_url and precedent_character == "-":
+            get_url = character == " "
+            if get_url:
+                continue
+        elif get_url:
+            url += character
+        precedent_character = character
+    if not url:
+        url = "no url"
+    del precedent_character, get_url
+    return description, url
+
+
+def load_packages_from_directory(directory, test="test.sh"):
     """Load packages from scripts."""
+    import glob
     directory = str(directory)
     packages_loaded = dict()
     os.chdir(directory)
@@ -26,29 +47,11 @@ def load_packages_from_directory(directory):
         with open(file, "r") as open_file:
             package_info = str(open_file.readline())
             if package_info[0] == "#":
-                package_info = package_info.strip(
-                    "\n").strip("# ")
-                for character in package_info:
-                    if character == "-":
-                        break
-                    package_description += character
-                package_url = ""
-                precedent_character = ""
-                get_url = False
-                for character in package_info:
-                    if not get_url and precedent_character == "-":
-                        get_url = character == " "
-                        if get_url:
-                            continue
-                    elif get_url:
-                        package_url += character
-                    precedent_character = character
-                if not package_url:
-                    package_url = "no url"
-                del precedent_character, get_url
+                package_description, package_url =
+                get_package_info(package_info)
             else:
                 package_description = package_url = package_name
-        if file == "test.sh":
+        if file == test:
             packages_loaded[0] = [
                 package_name,
                 package_description,
@@ -69,13 +72,8 @@ def load_packages_from_directory(directory):
 
 def make_file_from_packages(packages_list, file_name):
     """Create binary file from list."""
+    import pickle
     with open(file_name, "wb") as file:
         pickle.dump(packages_list, file, protocol=0)
         print("[LOG] Packages store into {}".format(file_name))
     return ""
-
-if os.path.exists(packages_binary_file):
-    packages = load_packages_from_file(packages_binary_file)
-else:
-    packages = load_packages_from_directory("scripts/")
-    make_file_from_packages(packages, packages_binary_file)
