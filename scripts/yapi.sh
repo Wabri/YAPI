a8e0d3321c271e23895544b89c73ac0986ae4cbe
IanDuncanT <duncan.ian.t@gmail.com>
2018-12-13 17:24:39 -0600

    Start update script and move code to functions

---
+++ scripts/yapi.sh
@@ -0,0 +1,3 @@
+# Install YAPI from Github - https://github.com/Wabri/YAPI
+cd ~
+git clone https://github.com/IanDuncanT/YAPI.git
--- yapi.py
+++ yapi.py
@@ -27,65 +27,16 @@ no_answer = ("N", "No", "n", "no")
 right_answer = yes_answer + no_answer
 options = {
     "install": ["<package_to_install>", "Install one of the packages"],
-    "console": ["no", "Run Yapi with the terminal question installer"]
+    "console": ["no", "Run Yapi with the terminal question installer"],
+    "update" : ["no", "Pull the newest YAPI version from github"],
+    "cache"  : ["no", "Recreate the cache"]
 }
 
 #Caching of Install Files
 if os.path.exists(packages_binary_file_store):
-    with open(packages_binary_file_store, "rb") as packages_binary:
-        packages = pickle.load(packages_binary)
-        print("Packages load from {}".format(packages_binary_file_store))
+    cacheOpen()
 else:
-    os.chdir(where_is_scripts)
-    counter_packages = 1
-    for file in glob.glob("*.sh"):
-        package_name = file.split(".")[0]
-        package_description = ""
-        with open(file, "r") as open_file:
-            package_info = str(open_file.readline())
-            if package_info[0] == "#":
-                package_info = package_info.strip(
-                    "\n").strip("# ")
-                for character in package_info:
-                    if character == "-":
-                        break
-                    package_description += character
-                package_url = ""
-                precedent_character = ""
-                get_url = False
-                for character in package_info:
-                    if not get_url and precedent_character == "-":
-                        get_url = character == " "
-                        if get_url:
-                            continue
-                    elif get_url:
-                        package_url += character
-                    precedent_character = character
-                if not package_url:
-                    package_url = "no url"
-                del precedent_character, get_url
-            else:
-                package_description = package_url = package_name
-        if file == "test.sh":
-            packages[0] = [
-                package_name,
-                package_description,
-                package_url,
-                where_is_scripts + file
-            ]
-        else:
-            packages[counter_packages] = [
-                package_name,
-                package_description,
-                package_url,
-                where_is_scripts + file
-            ]
-            counter_packages += 1
-    os.chdir("..")
-    with open(packages_binary_file_store, "wb") as packages_binary:
-        pickle.dump(packages, packages_binary,
-                    protocol=0)
-        print("Packages store into {}".format(packages_binary_file_store))
+    cacheCreate()
 
 try:
     class packageScreen(GridLayout):
@@ -138,74 +89,127 @@ if len(sys.argv) == 1:
 
 elif len(sys.argv) == 2:
     if (sys.argv[1] == "console"):
-        continue_to_ask = True
-        while continue_to_ask:
-            print("-" * 79)
-            print("You can choose to install this packages:")
-            for package_counter in packages:
-                print("{:>2}) {} - {}".format(
-                    package_counter,
-                    packages[package_counter][0].capitalize(),
-                    packages[package_counter][1]))
-            choose = -1
-            while choose not in range(package_counter + 1):
-                choose = input("What package do you want to install? ")
-                try:
-                    choose = int(choose)
-                    if choose not in range(package_counter + 1):
-                        print("The package number must be between " +
-                              " 0 and {}".format(package_counter))
-                except ValueError:
-                    if choose == "exit":
-                        print("Ok, bye bye!")
-                        exit()
-                    print("Please insert a number between 0 and {}".format(
-                        package_counter))
-                    choose = -1
-            package_to_install = choose
-            while choose not in right_answer:
-                choose = input(
-                    "Do you want to install {}? ".format(
-                        packages[package_to_install][0]))
-            if choose in yes_answer:
-                print("Let's start the installation, these take a moment...")
-                with open(packages[package_to_install][3], "r") as file_script:
-                    bashCommand = ""
-                    for line in file_script.readlines():
-                        if line[0] != "#":
-                            bashCommand += line
-                    bashCommand = bashCommand.replace("\n", " ; ")
-                    subprocess.call(
-                        bashCommand, stderr=subprocess.STDOUT, shell=True)
-            else:
-                print("Ok, no problem...")
-            choose = ""
-            while choose not in right_answer:
-                choose = input("Do you want to install something else? ")
-            if choose in yes_answer:
-                continue
-            else:
-                print("Ok, bye bye!")
-                exit()
-        print("-" * 79)
+        consoleInstall()
+    elif (sys.argv[1] == "update"):
+        installPackage(yapi)
+    elif (sys.argv[1] == "cache"):
+        cacheRemove()
+        cacheCreate()
+
     else:
-        print("The argument {} isn't allowed,".format(sys.argv[1].upper()) +
-              " you can choose from this arguments:")
-        for option in options:
-            if options[option][0] != "no":
-                print("\t - {} \n\t\t python yapi.py {} {}".format(
-                    options[option][1], option, options[option][0]))
-            else:
-                print("\t - {} \n\t\t python yapi.py {}".format(
-                    options[option][1], option))
+        argumentError(sys.argv[1])
 
 elif len(sys.argv) == 3:
     if sys.argv[1] == "install":
-        package_to_install = sys.argv[2]
-        print("Let's start the installation, these take a moment...")
-        try:
-            file_script = where_is_scripts + sys.argv[2] + ".sh"
-            with open(file_script, "r") as file_script:
+        installPackage(sys.argv[2])
+
+def installPackage(package):
+    try:
+        file_script = where_is_scripts + package + ".sh"
+        with open(file_script, "r") as file_script:
+            bashCommand = ""
+            for line in file_script.readlines():
+                if line[0] != "#":
+                    bashCommand += line
+            bashCommand = bashCommand.replace("\n", " ; ")
+            subprocess.call(
+                bashCommand, stderr=subprocess.STDOUT, shell=True)
+    except (OSError, IOError, KeyError) as e:
+        print("Package not found. Try again.")
+
+def cacheCreate():
+    os.chdir(where_is_scripts)
+    counter_packages = 1
+    for file in glob.glob("*.sh"):
+        package_name = file.split(".")[0]
+        package_description = ""
+        with open(file, "r") as open_file:
+            package_info = str(open_file.readline())
+            if package_info[0] == "#":
+                package_info = package_info.strip(
+                    "\n").strip("# ")
+                for character in package_info:
+                    if character == "-":
+                        break
+                    package_description += character
+                package_url = ""
+                precedent_character = ""
+                get_url = False
+                for character in package_info:
+                    if not get_url and precedent_character == "-":
+                        get_url = character == " "
+                        if get_url:
+                            continue
+                    elif get_url:
+                        package_url += character
+                    precedent_character = character
+                if not package_url:
+                    package_url = "no url"
+                del precedent_character, get_url
+            else:
+                package_description = package_url = package_name
+        if file == "test.sh":
+            packages[0] = [
+                package_name,
+                package_description,
+                package_url,
+                where_is_scripts + file
+            ]
+        else:
+            packages[counter_packages] = [
+                package_name,
+                package_description,
+                package_url,
+                where_is_scripts + file
+            ]
+            counter_packages += 1
+    os.chdir("..")
+    with open(packages_binary_file_store, "wb") as packages_binary:
+        pickle.dump(packages, packages_binary,
+                    protocol=0)
+        print("Packages store into {}".format(packages_binary_file_store))
+
+def cacheRemove():
+    os.remove("packages.bin")
+
+def cacheOpen():
+    with open(packages_binary_file_store, "rb") as packages_binary:
+        packages = pickle.load(packages_binary)
+        print("Packages load from {}".format(packages_binary_file_store))
+
+def consoleInstall():
+    continue_to_ask = True
+    while continue_to_ask:
+        print("-" * 79)
+        print("You can choose to install this packages:")
+        for package_counter in packages:
+            print("{:>2}) {} - {}".format(
+                package_counter,
+                packages[package_counter][0].capitalize(),
+                packages[package_counter][1]))
+        choose = -1
+        while choose not in range(package_counter + 1):
+            choose = input("What package do you want to install? ")
+            try:
+                choose = int(choose)
+                if choose not in range(package_counter + 1):
+                    print("The package number must be between " +
+                          " 0 and {}".format(package_counter))
+            except ValueError:
+                if choose == "exit":
+                    print("Ok, bye bye!")
+                    exit()
+                print("Please insert a number between 0 and {}".format(
+                    package_counter))
+                choose = -1
+        package_to_install = choose
+        while choose not in right_answer:
+            choose = input(
+                "Do you want to install {}? ".format(
+                    packages[package_to_install][0]))
+        if choose in yes_answer:
+            print("Let's start the installation, these take a moment...")
+            with open(packages[package_to_install][3], "r") as file_script:
                 bashCommand = ""
                 for line in file_script.readlines():
                     if line[0] != "#":
@@ -213,5 +217,25 @@ elif len(sys.argv) == 3:
                 bashCommand = bashCommand.replace("\n", " ; ")
                 subprocess.call(
                     bashCommand, stderr=subprocess.STDOUT, shell=True)
-        except (OSError, IOError, KeyError) as e:
-            print("Package not found. Try again.")
+        else:
+            print("Ok, no problem...")
+        choose = ""
+        while choose not in right_answer:
+            choose = input("Do you want to install something else? ")
+        if choose in yes_answer:
+            continue
+        else:
+            print("Ok, bye bye!")
+            exit()
+    print("-" * 79)
+
+def argumentError(arg):
+    print("The argument {} isn't allowed,".format(arg.upper()) +
+          " you can choose from this arguments:")
+    for option in options:
+        if options[option][0] != "no":
+            print("\t - {} \n\t\t python yapi.py {} {}".format(
+                options[option][1], option, options[option][0]))
+        else:
+            print("\t - {} \n\t\t python yapi.py {}".format(
+                options[option][1], option))
