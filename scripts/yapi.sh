86e1d3fe6900a41845a2d11e50af78f8156df4a3
Gabriele Puliti <gabriele.puliti@gmail.com>
2018-12-15 00:10:31 +0100

    update

--- scripts/yapi.sh
+++ scripts/yapi.sh
@@ -1,5 +1,5 @@
 # Install YAPI from Github - https://github.com/Wabri/YAPI
 git clone https://github.com/Wabri/YAPI.git --depth 1
-sudo rm -r YAPI/.git YAPI/.gitignore YAPI/.travis.yml YAPI/readme_updater.py
-./YAPI/yapi.sh help
 cd YAPI
+sudo rm -r .git .gitignore .travis.yml readme_updater.py
+./yapi.sh help
