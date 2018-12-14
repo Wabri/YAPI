30f3a9e8cf428238691f3c6fe4b18fd1d3c41d16
Gabriele Puliti <gabriele.puliti@gmail.com>
2018-12-15 00:11:51 +0100

    not need delete useless file

--- scripts/yapi.sh
+++ scripts/yapi.sh
@@ -1,5 +1,3 @@
 # Install YAPI from Github - https://github.com/Wabri/YAPI
 git clone https://github.com/Wabri/YAPI.git --depth 1
-cd YAPI
-sudo rm -r .git .gitignore .travis.yml readme_updater.py
 ./yapi.sh help
