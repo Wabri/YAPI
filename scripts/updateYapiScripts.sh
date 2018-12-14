244a6b58a2a7a5b06e756f8d1e4f81e696837e15
Gabriele Puliti <gabriele.puliti@gmail.com>
2018-12-15 00:28:06 +0100

    solved typo in yapi update script

--- scripts/updateYapiScripts.sh
+++ scripts/updateYapiScripts.sh
@@ -1,5 +1,5 @@
 # Update scripts from Github - https://github.com/Wabri/YAPI
 rm -r scripts
-git clone https://github.com/Wabri/YAPI.git` --depth 1
+git clone https://github.com/Wabri/YAPI.git --depth 1
 mv YAPI/scripts .
 sudo rm -r YAPI
