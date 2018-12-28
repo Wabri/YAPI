a7fa675ecda3291f7fae81ba10749694658c6541
Gabriele Puliti <gabriele.puliti@gmail.com>
2018-12-28 15:54:19 +0100

    added an install.sh script to install yapi

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
+cd ~
+git clone https://github.com/YetAnotherPackageInstaller/YAPI.git --depth 1
+cd YAPI
+sudo rm -r .git* .travis.yml install.sh readme_updater.py
 sudo ln -s ~/YAPI/yapi.sh /usr/local/bin/yapi
