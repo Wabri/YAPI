2c1b5146168a11e07f385faca2fdd9af598ae0cc
Gabriele Puliti <gabriele.puliti@gmail.com>
2019-01-06 17:43:13 +0100

    added make on dependencies

--- scripts/i3gaps.sh
+++ scripts/i3gaps.sh
@@ -1,4 +1,5 @@
 # i3 gaps is i3 with more features - https://github.com/Airblader/i3
+sudo apt install make
 sudo apt install libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0-dev
 # clone the repository
 git clone https://www.github.com/Airblader/i3 i3-gaps
