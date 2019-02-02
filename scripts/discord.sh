d2e0871794b9a705410eec29bd41adc5da668f4d
Gabriele Puliti <gabriele.puliti@gmail.com>
2019-02-02 13:18:19 +0100

    Update (discord): set english description

--- scripts/discord.sh
+++ scripts/discord.sh
@@ -1,4 +1,4 @@
-# Discord chat vocale e testuale - https://discordapp.com/
+# Discord chat vocal and text - https://discordapp.com/
 wget -O discord.deb https://discordapp.com/api/download?platform=linux&format=deb
 sudo dpkg -i discord.deb
 rm -r discord.deb
