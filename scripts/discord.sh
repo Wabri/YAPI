45f10290bfaee55cf23d7780b4b9d0f5bd7951f3
Ian Duncan <duncan.ian.t@gmail.com>
2019-02-03 15:50:31 -0600

    Use voice instead of vocal
    Discord uses voice chat to describe it. Vocal often is related to singing.

--- scripts/discord.sh
+++ scripts/discord.sh
@@ -1,4 +1,4 @@
-# Discord vocal and text chat - https://discordapp.com/
+# Discord voice and text chat - https://discordapp.com/
 wget -O discord.deb https://discordapp.com/api/download?platform=linux&format=deb
 sudo dpkg -i discord.deb
 rm -r discord.deb
