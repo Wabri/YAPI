982a3ecf4a43003d2d774017b16766af75e92665
Gabriele Puliti <gabriele.puliti@gmail.com>
2019-02-08 22:49:38 +0100

    Add (visualStudioCode.sh): new script added
    Closes #101

---
+++ scripts/visualStudioCode.sh
@@ -0,0 +1,4 @@
+# Visual Studio Code is a lightweight but powerful source code editor - https://code.visualstudio.com/
+wget -O visualstudiocode.deb https://go.microsoft.com/fwlink/?LinkID=760868
+sudo dpkg -i visualstudiocode.deb
+rm -r visualstudiocode.deb
