9785ab0bc3d37c3ca65d3f6911a73d5e31e1265e
GabrielePuliti <gabriele.puliti@gmail.com>
2019-03-18 16:57:45 +0100

    Update etcher.sh

--- scripts/etcher.sh
+++ scripts/etcher.sh
@@ -1,4 +1,4 @@
-# Ramme unofficial Instagram Desktop App - https://github.com/balena-io/etcher
+# Etcher flash OS images to SD cards & USB drives, safely and easily - https://github.com/balena-io/etcher
 wget -O etcher.deb https://github.com/balena-io/etcher/releases/download/v1.5.5/balena-etcher-electron_1.5.5_amd64.deb
 sudo dpkg -i etcher.deb
 rm etcher.deb
