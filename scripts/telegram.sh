030dca9e20952ab2c9bad8eef182a0ff3d5aed1c
Gabriele Puliti <gabriele.puliti@gmail.com>
2019-03-28 16:07:46 +0100

    Fix (scripts/telegram): using telegram website instead of github

--- scripts/telegram.sh
+++ scripts/telegram.sh
@@ -1,9 +1,5 @@
 # Telegram Fast and secure desktop app, perfectly synced with your mobile phoneDesktop - https://desktop.telegram.org/
-curl --get https://api.github.com/repos/telegramdesktop/tdesktop/releases/latest | grep "browser_download" | grep "tar.xz" | tr -d '"'  > tout
-cut -c 29- tout > tlast
-rm tout
-wget -O telegram.tar.xz -i tlast
-rm tlast
+wget -O telegram.tar.xz https://telegram.org/dl/desktop/linux
 tar xf telegram.tar.xz
 sudo mv Telegram/Telegram /usr/local/bin/telegram
 rm -r ?elegram*
