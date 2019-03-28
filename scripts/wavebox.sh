9d5afc80cdea19044a49f858f5aa9c958ed96656
Gabriele Puliti <gabriele.puliti@gmail.com>
2019-03-28 15:43:45 +0100

    Fix (scripts/telegram & scripts/wavebox): auto get the last version of telegram and fix wavebox script

--- scripts/wavebox.sh
+++ scripts/wavebox.sh
@@ -5,3 +5,5 @@ rm wavebox.key
 echo "deb https://wavebox.io/dl/client/repo/ x86_64/" | sudo tee -a /etc/apt/sources.list.d/repo.list
 sudo apt update
 sudo apt install wavebox
+echo "/opt/wavebox/Wavebox --mailto=%u \> /dev/null" > /usr/local/bin/wavebox
+chmod +x /usr/local/bin/wavebox
