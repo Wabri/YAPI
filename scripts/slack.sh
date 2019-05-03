0b42ecf532601108af1f573988fe477e8fade1e5
GabrielePuliti <gabriele.puliti@gmail.com>
2019-05-03 10:14:39 +0200

    Update slack.sh

--- scripts/slack.sh
+++ scripts/slack.sh
@@ -1,4 +1,5 @@
 # Slack can help your team work together and get things done - https://slack.com/
-wget -O slack.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.3-amd64.deb
+wget -O slack.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-3.4.0-amd64.deb
 sudo dpkg -i slack.deb
+sudo apt --fix-broken install
 rm -r slack.deb
