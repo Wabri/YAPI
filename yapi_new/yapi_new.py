54734308cbec0202d08b8e8f3f8e2a566da6bae3
Wabri <gabriele.puliti@gmail.com>
2019-06-24 14:52:13 +0200

    Add (languages/en): create language pack

--- yapi_new/yapi_new.py
+++ yapi_new/yapi_new.py
@@ -1,33 +1,48 @@
 
+def _remove_whitespaces(string):
+    while ', ' in string:
+        string = string.replace(', ', ',')
+    return string
+
 if __name__ == '__main__':
 
+    from configparser import ConfigParser
+
+
+    lang_path = 'languages/en'
+    lang = ConfigParser()
+    lang.read(lang_path)
+    get_default_lang = lambda config, key: config['DEFAULT'][key]
+
     print('-'*50)
-    print('Hello this is YAPI new version')
+    print(get_default_lang(lang,'hello'))
     print('-'*50)
 
     while True:
-        print('What do you want to do?')
 
-        print(' - Install/update packages')
-        install_update_request = ['install', 'update', 'install packages', 'update packages']
 
-        print(' - Change configuration')
-        change_config_request = ('configuration', 'config', 'change', 'change config', 'change configuration')
+        print(get_default_lang(lang,'post_config_question_0'))
 
-        print(' - Exit')
-        exit_request = ('exit', 'quit')
+        print(' - ' + get_default_lang(lang,'answer_question0_0'))
+        install_update_request = _remove_whitespaces(get_default_lang(lang,'user_answers_answer0')).split(',')
+
+        print(' - ' + get_default_lang(lang, 'answer_question0_1'))
+        change_config_request = _remove_whitespaces(get_default_lang(lang, 'user_answers_answer1')).split(',')
+
+        print(' - ' + get_default_lang(lang, 'answer_question0_2'))
+        exit_request = _remove_whitespaces(get_default_lang(lang, 'user_answers_answer2')).split(',')
 
         choose = (str)(input('-----> ')).lower()
 
         if choose in exit_request:
-            print('Bye')
+            print(get_default_lang(lang, 'answer_user_question0_2'))
             exit()
         elif choose in install_update_request:
-            print('Install update packages')
+            print(get_default_lang(lang, 'answer_user_question0_0'))
         elif choose in change_config_request:
-            print('Change config')
+            print(get_default_lang(lang, 'answer_user_question0_1'))
         else:
-            print("Sorry I don't understand")
+            print(get_default_lang(lang, 'answer_user_understand'))
 
         print('-'*50)
 
