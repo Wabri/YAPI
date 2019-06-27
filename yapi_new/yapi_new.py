9133a9b378349750b20a0bda04de0c69fba4276a
Wabri <gabriele.puliti@gmail.com>
2019-06-27 10:47:26 +0200

    Update (yapi_new): language configuration set up

--- yapi_new/yapi_new.py
+++ yapi_new/yapi_new.py
@@ -18,23 +18,40 @@ if __name__ == '__main__':
     config.read(config_path)
 
     if get_default(config,'language_use') is '':
-        lang_dict = []
+        lang_list = []
+        print('-'*50)
         print(get_default(lang,'lang_missing'))
-        for file in glob.glob(lang_path+'*'):
+        print('-'*50)
+        for file in glob.glob(lang_path +'*'):
             lang_file = (str)(file)
-            print(lang_file)
-            lang.read(lang_path + lang_file)
-            lang_name = get_default(lang,'name')
-            print(lang_name)
-            lang_dict.append([lang_file.replace(lang_path,''),lang_name])
-            lang.clear()
+            lang_temp = ConfigParser()
+            lang_temp.read(lang_file)
+            lang_name = get_default(lang_temp,'name')
+            lang_list.append([lang_file.replace(lang_path,'').lower(),lang_name])
+            del lang_temp, lang_file, lang_name
+        not_choose = True
+        while not_choose:
+            print('Select language')
+            for language in lang_list:
+                print(' - {} ({})'.format(language[1].capitalize(), language[0]))
+            choose = (str)(input('-----> ')).lower()
+            for language in lang_list:
+                if choose in language[0].lower() or choose in language[1].lower():
+                    lang_temp = ConfigParser()
+                    config.set('DEFAULT', 'language_use', language[0])
+                    with open(config_path, 'w') as configfile:
+                        config.write(configfile)
+                    not_choose = False
+                    print('You choose {} language'.format(choose))
+                    lang.read(lang_path + language[0])
+    else:
+        lang.read(lang_path+get_default(config, 'language_use'))
 
     print('-'*50)
     print(get_default(lang,'hello'))
     print('-'*50)
 
     while True:
-
 
         print(get_default(lang,'post_config_question_0'))
 
