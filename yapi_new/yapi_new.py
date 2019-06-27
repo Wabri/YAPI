09cc4805a3ccee02ceccef72418069b218b47db2
Wabri <gabriele.puliti@gmail.com>
2019-06-27 19:58:59 +0200

    Add (yapi_new.py): extract function from main procedure into _install_procedure

--- yapi_new/yapi_new.py
+++ yapi_new/yapi_new.py
@@ -1,6 +1,9 @@
 from configparser import ConfigParser
 import glob
 
+
+_packages_path = 'packages/'
+
 _lenght_separator = 50
 _separator = '-'
 
@@ -12,8 +15,41 @@ def _remove_whitespaces(string):
         string = string.replace(', ', ',')
     return string
 
+def _install_procedure(lang, packages_path=_packages_path):
+    while True:
+        files_list = []
+
+        for file in glob.glob(packages_path + '*'):
+            file_name = file.strip(packages_path).strip('.yp').strip('.sh')
+            print(' - ' + file_name)
+            files_list.append(file_name)
+        print(' - ' + _get_default(lang, 'main0_title_exit'))
+
+        choose = (str)(input('-----> ')).lower()
+
+        print(_get_default(lang,'main2_title_install'))
+
+        if choose in files_list:
+            start_install =_get_default(lang,'main3_start_install').format(choose.capitalize())
+            end_install = _get_default(lang,'main3_end_install').format(choose.capitalize())
+            max_string = max(len(start_install),len(end_install))
+            _print_separator(lenght=max_string)
+            print(start_install)
+            print(end_install)
+            _print_separator(lenght=max_string)
+            del start_install, end_install, max_string
+        elif choose in exit_request:
+            break
+        else:
+            title_undestand = _get_default(lang, 'main1_title_undestand')
+            _print_separator(lenght=len(title_undestand))
+            print(title_undestand)
+            _print_separator(lenght=len(title_undestand))
+            del title_undestand
+
 def _config_language(lang, lang_path):
     lang_list = []
+
     for file in glob.glob(lang_path +'*'):
         lang_file = (str)(file)
         lang_temp = ConfigParser()
@@ -21,12 +57,16 @@ def _config_language(lang, lang_path):
         lang_name = _get_default(lang_temp,'name')
         lang_list.append([lang_file.replace(lang_path,'').lower(),lang_name])
         del lang_temp, lang_file, lang_name
+
     not_choose = True
     while not_choose:
         print(_get_default(lang,'conf1_title_language'))
+
         for language in lang_list:
             print(' - {} ({})'.format(language[1].capitalize(), language[0]))
+
         choose = (str)(input('-----> ')).lower()
+
         for language in lang_list:
             if choose in language[0].lower() or choose in language[1].lower():
                 lang_temp = ConfigParser()
@@ -39,10 +79,9 @@ def _config_language(lang, lang_path):
 
 if __name__ == '__main__':
 
-    packages_path = 'packages/'
     lang_path = 'languages/'
     lang = ConfigParser()
-    lang.read(lang_path+'en')
+    lang.read(lang_path + 'en')
 
     config_path = 'config'
     config = ConfigParser()
@@ -83,47 +122,49 @@ if __name__ == '__main__':
             print(_get_default(lang, 'main1_title_exit'))
             exit()
         elif choose in install_update_request:
-            title_install = _get_default(lang, 'main1_title_install')
-            _print_separator(lenght=len(title_install))
-            print(title_install)
-            _print_separator(lenght=len(title_install))
-            del title_install
             while True:
-                files_list = []
-                for file in glob.glob(packages_path + '*'):
-                    file_name = file.strip(packages_path).strip('.yp').strip('.sh')
-                    print(' - ' + file_name)
-                    files_list.append(file_name)
+                title_install = _get_default(lang, 'main1_title_install')
+
+                _print_separator(lenght=len(title_install))
+                print(title_install)
+                _print_separator(lenght=len(title_install))
+                del title_install
+
+                print(' - ' + _get_default(lang,'main1_list_install'))
+                list_install = _remove_whitespaces(_get_default(lang,'main1_list_answers_install')).split(',')
+
+                print(' - ' + _get_default(lang,'main1_categorize_install'))
+                category_install = _remove_whitespaces(_get_default(lang,'main1_categorize_answers_install')).split(',')
+
                 print(' - ' + _get_default(lang, 'main0_title_exit'))
+                exit_request = _remove_whitespaces(_get_default(lang, 'main0_answers_exit')).split(',')
+
                 choose = (str)(input('-----> ')).lower()
-                print(_get_default(lang,'main2_title_install'))
-                if choose in files_list:
-                    start_install =_get_default(lang,'main2_start_install').format(choose.capitalize())
-                    end_install = _get_default(lang,'main2_end_install').format(choose.capitalize())
-                    max_string = max(len(start_install),len(end_install))
-                    _print_separator(lenght=max_string)
-                    print(start_install)
-                    print(end_install)
-                    _print_separator(lenght=max_string)
-                    del start_install, end_install, max_string
-                elif choose in exit_request:
+
+                if choose in exit_request:
                     break
+                elif choose in list_install:
+                    _install_procedure(lang)
+                elif choose in category_install:
+                    print('Which category you want to choose?')
                 else:
-                    title_undestand = _get_default(lang, 'main1_title_undestand')
-                    _print_separator(lenght=len(title_undestand))
-                    print(title_undestand)
-                    _print_separator(lenght=len(title_undestand))
+                    print(_get_default(lang, 'main1_title_undestand'))
+
         elif choose in change_config_request:
             title_configuration = _get_default(lang, 'main1_title_configuration')
             _print_separator(lenght=len(title_configuration))
             print(title_configuration)
             _print_separator(lenght=len(title_configuration))
+            del title_configuration
+
             while True:
                 for items in config.items():
                     for item in items[1]:
                         print(' - ' + item)
                     print(' - ' + _get_default(lang, 'main0_title_exit'))
+
                 choose = (str)(input('-----> ')).lower()
+
                 if choose == 'language_use':
                     _config_language(lang,lang_path)
                     break
